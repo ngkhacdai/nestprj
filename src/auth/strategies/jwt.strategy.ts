11d6fa3158aa8fea07f17a6478c21331ece7f09c
ngkhacdai <ngkhacdai@gmail.com>
2024-06-17 17:14:42 +0700

    auth

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,17 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: 'abc123',
+        });
+    }
+    validate(payload: any) {
+        return payload
+    }
+}
